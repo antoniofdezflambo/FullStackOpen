c9b2c78faae015242a91229a1324d61113678e8b
antoniofdezflambo <antoniofdezflambo@gmail.com>
2024-03-27 03:52:17 +0100

    Exercises 2.4 & 2.5 completed

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,10 +1,11 @@
+import Header from "./Header"
 import Part from "./Part"
 import Total from "./Total"
 
 const Course = ({course}) => {
     return (
         <>
-            <h1>{course.name}</h1>
+            <Header name={course.name} />
             {
                 course.parts.map(part =>
                     <Part key={part.id} name={part.name} exercises={part.exercises} />
